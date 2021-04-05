c2aaf3a20684b7adb1a29ebe0f2aca2984d5ac84
acras01 <acras1@gmail.com>
2021-04-05 09:00:43 +0300

    Rewrite to only doze app: remove all obsolete code, localize and adapt to soong build system

---
+++ src/me/phh/treble/app/SettingsActivity.kt
@@ -0,0 +1,102 @@
+package me.phh.treble.app
+
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Bundle
+import android.preference.PreferenceActivity
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragment
+import androidx.preference.PreferenceManager
+
+/**
+ * A [PreferenceActivity] that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ *
+ * See [Android Design: Settings](http://developer.android.com/design/patterns/settings.html)
+ * for design guidelines and the [Settings API Guide](http://developer.android.com/guide/topics/ui/settings.html)
+ * for more information on developing a Settings UI.
+ */
+class SettingsActivity : PreferenceActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupActionBar()
+        getFragmentManager().beginTransaction().replace(android.R.id.content, DozeSettingsFragment()).commit();
+    }
+
+    /**
+     * Set up the [android.app.ActionBar], if the API is available.
+     */
+    private fun setupActionBar() {
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+//        actionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onIsMultiPane(): Boolean {
+        return isXLargeTablet(this)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+//    override fun onBuildHeaders(target: MutableList<Header>) {
+//        loadHeadersFromResource(R.xml.pref_headers, target)
+//    }
+
+    /**
+     * This method stops fragment injection in malicious applications.
+     * Make sure to deny any unknown fragments here.
+     */
+    override fun isValidFragment(fragmentName: String): Boolean {
+        return PreferenceFragment::class.java.name == fragmentName
+                || DozeSettingsFragment::class.java.name == fragmentName
+    }
+
+    companion object {
+        /**
+         * A preference value change listener that updates the preference's summary
+         * to reflect its new value.
+         */
+        private val sBindPreferenceSummaryToValueListener =
+            Preference.OnPreferenceChangeListener { preference, value ->
+                preference.summary = value.toString()
+                true
+            }
+
+        /**
+         * Helper method to determine if the device has an extra-large screen. For
+         * example, 10" tablets are extra-large.
+         */
+        private fun isXLargeTablet(context: Context): Boolean {
+            return context.resources.configuration.screenLayout and
+                    Configuration.SCREENLAYOUT_SIZE_MASK >= Configuration.SCREENLAYOUT_SIZE_XLARGE
+        }
+
+        /**
+         * Binds a preference's summary to its value. More specifically, when the
+         * preference's value is changed, its summary (line of text below the
+         * preference title) is updated to reflect the value. The summary is also
+         * immediately updated upon calling this method. The exact display format is
+         * dependent on the type of preference.
+
+         * @see .sBindPreferenceSummaryToValueListener
+         */
+        fun bindPreferenceSummaryToValue(preference: Preference) {
+            // Set the listener to watch for value changes.
+            preference.onPreferenceChangeListener = sBindPreferenceSummaryToValueListener
+
+            // Trigger the listener immediately with the preference's current value.
+            sBindPreferenceSummaryToValueListener.onPreferenceChange(
+                preference,
+                PreferenceManager
+                    .getDefaultSharedPreferences(preference.context)
+                    .getString(preference.key, "")
+            )
+        }
+    }
+}
